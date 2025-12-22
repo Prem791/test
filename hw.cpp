d3e7461f284f6f7199ae93a7a601edb6f0865de3
prem patel <patelprem031005@gmail.com>
2025-12-22 15:50:12 +0530

    new make file and add func

--- hw.cpp
+++ hw.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 
+using std::cin;
 using std::cout;
 using std::endl;
 
+class Calc{
+  public:
+    int add(int x, int y){
+      return x+y;
+    }
+};
+
 int main () {
-  cout << "Hello World" << endl;
+  int x, y;
+  cout << "Enter 1st number: ";
+  cin >> x;
+  cout << "Enter 2nd number: ";
+  cin >> y;
+  
+  Calc calc;
+  cout << "Sum: " << calc.add(x, y) << endl;
+  
   return 0;
 }
